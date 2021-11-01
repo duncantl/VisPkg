9b00308efe7dc6ea2d673da62584f8cd185fd460
Duncan Temple Lang <duncan@r-project.org>
2021-10-31 18:55:36 -0700

    moved an old version of computeFileLineCoords which was for lines, not rectangles, to notUsed.R

--- notUsed.R
+++ notUsed.R
@@ -16,3 +16,32 @@ function(coords, col = coords[, 3], ...)
            }, groups, names(groups))
    
 }
+
+
+
+if(FALSE) {
+computeFileLineCoords =
+function(elInfo, left, right, bottom, top = 1)
+{
+
+    if(is.data.frame(elInfo))
+        elInfo = elInfo[,1]
+    
+    # elInfo will initially be the number of characters per line
+    # so we want to draw each line between left and right proportionally
+    # to that length.
+
+    # lines(c(10, 10.5, NA, 10, 10.6) + 1, c(.9, .9, NA, .8, .8), col = "red")
+    len = length(elInfo)
+    yv = seq(top, bottom, length = len)
+    y = x = rep(NA, 3 * len)
+    i = seq(1, by = 1, length = 3*len)
+    y[ (i %% 3 != 0) ] = rep(yv, each = 2)
+
+    i = seq(1, by = 3, length = len)
+    x[i] = left
+    x[i+1] = left + (right - left)*elInfo/max(elInfo)
+      
+    cbind(x, y)
+}
+}
