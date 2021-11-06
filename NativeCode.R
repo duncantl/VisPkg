dbd21e3566fe0b3fcae100eb3853a58bf14238be
Duncan Temple Lang <duncan@r-project.org>
2021-11-06 10:59:00 -0700

    improvements

--- NativeCode.R
+++ NativeCode.R
@@ -3,7 +3,9 @@ function(filename= NA) {
     cursors = integer()
     files = character()
     names = character()
-    lines = character()        
+    lines = character()
+    types = character()
+    
     update = function(cur, parent) {
         kind = getCursorKind(cur)
 
@@ -18,6 +20,7 @@ function(filename= NA) {
             return(update(children(cur)[[1]]))
 
         cursors[[ length(cursors) + 1L]] <<- kind
+        types[[ length(types) + 1L ]] <<- if(isCRoutineDef(cur, kind)) "FunctionDefn" else mapCursorTypes(kind)
         files[[ length(files) + 1L]] <<- getFileName(cur)
         names[[ length(names) + 1L]] <<- getName(cur)
         lines[[ length(lines) + 1L]] <<- getLocation(cur)$location["line"]
@@ -26,7 +29,20 @@ function(filename= NA) {
         CXChildVisit_Continue
     }
 
-    list(update = update, result = function() data.frame(type = mapCursorTypes(cursors), file = files, name = names, lineNumber = lines, kind = cursors))
+    list(update = update, result = function() data.frame(value = rep(1, length(cursors)), type = types, #mapCursorTypes(cursors),
+                                                         file = files, name = names, lineNumber = lines, kind = cursors))
+}
+
+isCRoutineDef =
+function(cur, kind = getCursorKind(cur))
+{
+    if(kind == CXCursor_FunctionDecl) {
+        k = children(cur)
+        if(length(k) > 0 && getCursorKind(k[[length(k)]]) == CXCursor_CompoundStmt)
+            return(TRUE)
+    }
+
+    FALSE
 }
 
 isExternC =
@@ -38,7 +54,7 @@ function(cur)
 
 mapCursorTypes =
 function(vals)
-    names(CXCursorKind)[match(vals, CXCursorKind)]
+    gsub("^CXCursor_", "", names(CXCursorKind)[match(vals, CXCursorKind)])
 
 
 getToplevelNativeInfo =
@@ -52,13 +68,18 @@ function(x, ..., col = genNativeTypeCollector(getFileName(x)))
     d[ d$file == getFileName(x), ]
 }
 
+
+NativeCodeTypeMap = c("Constructor" = "green", EnumDecl = "gold", "FunctionDefn" = "red", "FunctionDecl" = "grey", "StructDecl" = "blue", "TypedefDecl" = "lightblue", "VarDecl" = "brown")
+
 if(FALSE) {
     xml.includes = includes = c("/usr/local/include/libxml2", "~/R/R-new/build3/include", "/Users/duncan/LLVM/clang+llvm-13.0.0-x86_64-apple-darwin/lib/clang/13.0.0/include")
     xml.args = c("-DUSER_OBJECT_=SEXP", "-DLIBXML", "-DUSE_EXTERNAL_SUBSET=1", "-DROOT_HAS_DTD_NODE=1", "-DDUMP_WITH_ENCODING=1", "-DUSE_XML_VERSION_H=1", "-DXML_ELEMENT_ETYPE=1", "-DXML_ATTRIBUTE_ATYPE=1", "-DNO_XML_HASH_SCANNER_RETURN=1", "-DLIBXML_NAMESPACE_HAS_CONTEXT=1", "-DHAVE_R_CETYPE_T=1", "-DHAVE_XML_WITH_ZLIB=1", "-DHAVE_XML_HAS_FEATURE=1", "-DUSE_R=1", "-D_R_=1", "-DHAVE_VALIDITY=1", "-DXML_REF_COUNT_NODES=1", "-DLIBXML2=1")        
     tu = createTU("~/GitWorkingArea/XML/src/DocParse.c", includes = xml.includes, args = xml.args)
 
-    cfiles = xpdFileNames("~/GitWorkingArea/XML/src", pattern = "\\.c")
-    nativeInfo = lapply(cfiles, getToplevelNativeInfo, includes = xml.includes, args = xml.args)
+    xml.cfiles = xpdFileNames("~/GitWorkingArea/XML/src", pattern = "\\.c")
+    xml.nativeInfo = lapply(xml.cfiles, getToplevelNativeInfo, includes = xml.includes, args = xml.args)
+    names(xml.nativeInfo) = xml.cfiles
+    origShowFiles(vals = xml.nativeInfo, colorMap = NativeCodeTypeMap)
 }
 
 if(FALSE)
@@ -73,6 +94,8 @@ if(FALSE)
     files = xpdFileNames("~/GitWorkingArea/Rllvm/src", pattern = "\\.(cpp|C|c)$")
     z2 = lapply(files, getToplevelNativeInfo, includes = Rllvm.includes, args = Rllvm.args)
     names(z2) = files
+
+    origShowFiles(vals = z2, colorMap = NativeCodeTypeMap)    
     # gets a 42x3 data.frame  But the types and names are mostly UnexposedDecl and ""
     # These are extern "C" declarations.
     # Now fixed via the update function analyzing these.
